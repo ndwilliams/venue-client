56d6f8fff0833b187a9d8ac88bb08052a8252d5f
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-01-06 12:45:06 -0600

    added AddVenue routes and component

--- src/components/venues/AddVenue.jsx
+++ src/components/venues/AddVenue.jsx
@@ -1,3 +1,122 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export const AddVenue = () => {
-	return <div>Butts</div>
+	const [newVenue, setNewVenue] = useState({
+		name: "",
+		address: "",
+		venue_outside_image_url: "",
+		venue_inside_image_url: "",
+		capacity: "",
+		about_section: "",
+		active: true,
+	})
+	const navigate = useNavigate()
+
+	const addVenue = async () => {
+		const response = await fetch(`http://localhost:8000/venues`, {
+			method: "POST",
+			headers: {
+				Authorization: `Token ${localStorage.getItem("auth_token")}`,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newVenue),
+		})
+		return response
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		if (Object.values(newVenue).every(Boolean)) {
+			alert("Venue Successfully Added!")
+			await addVenue()
+			navigate(`/venues`)
+		} else {
+			alert("Please Fill Out All The Necessary Fields")
+		}
+	}
+
+	const handleInputChange = (event) => {
+		const venueCopy = { ...newVenue }
+		venueCopy[event.target.name] = event.target.value
+		setNewVenue(venueCopy)
+	}
+
+	return (
+		<form className="edit-venue-form">
+			<fieldset className="edit-venue-fieldset">
+				<label>Name:</label>
+				<input
+					name="name"
+					value={newVenue.name}
+					type="text"
+					className="venue-name-input"
+					placeholder="Venue Name"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset className="edit-venue-fieldset">
+				<label>Address:</label>
+				<input
+					name="address"
+					value={newVenue.address}
+					type="text"
+					className="venue-address-input"
+					placeholder="Venue Address"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset className="edit-venue-fieldset">
+				<label>Outside Image URL:</label>
+				<input
+					name="venue_outside_image_url"
+					value={newVenue.venue_outside_image_url}
+					type="url"
+					className="venue-outside-image-input"
+					placeholder="Venue Outside Image URL"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset className="edit-venue-fieldset">
+				<label>Inside Image URL:</label>
+				<input
+					name="venue_inside_image_url"
+					value={newVenue.venue_inside_image_url}
+					type="url"
+					className="venue-inside-image-input"
+					placeholder="Venue Inside Image URL"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset className="edit-venue-fieldset">
+				<label>Max Capacity:</label>
+				<input
+					name="capacity"
+					value={newVenue.capacity}
+					className="venue-max-capacity"
+					placeholder="Max Capacity"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset className="edit-venue-fieldset">
+				<label>About Section</label>
+				<textarea
+					name="about_section"
+					value={newVenue.about_section}
+					className="venue-about-section"
+					type="textarea"
+					placeholder="About Section"
+					onChange={handleInputChange}
+				/>
+			</fieldset>
+			<fieldset>
+				<button
+					type="submit"
+					className="venue-submit-button"
+					onClick={handleSubmit}>
+					Add Venue
+				</button>
+			</fieldset>
+		</form>
+	)
 }
